1776a6d0f58ebd574eb502e641393f656b8307f8
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-12-27 08:57:02 +0900

    Edgeでの動作を確認

--- src/background.ts
+++ src/background.ts
@@ -19,8 +19,34 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       console.log(res);
       const code = res?.split("&code=")[1];
       console.log(code);
-      sendResponse(code);
+      fetch("https://api.twitter.com/2/oauth2/token", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          Authorization: `Basic ZFU5TVFYTk9kMlpFWTJaeVkxZHZNR3BCTmxnNk1UcGphUToyRVNFR3h3T0dXQ09Vdjl4OHNMWTA4UnBTMVNzQUllNXloaEE3UTlsdU9INFlfQUw2Ug==`,
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          code: code!,
+          grant_type: "authorization_code",
+          client_id: API_KEY,
+          redirect_uri: REDIRECT_URL,
+          code_verifier: "abcd",
+        }),
+      })
+        .then((res) => {
+          console.log(res);
+          try {
+          } catch (e) {
+            console.log(e);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
   );
+  sendResponse("");
   return true;
 });
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 const API_KEY = "dU9MQXNOd2ZEY2ZyY1dvMGpBNlg6MTpjaQ";
 const REDIRECT_URL =
   "https://njiphocbohlfjlgediiblogkjpknelih.chromiumapp.org/callback";
@@ -40,30 +41,25 @@ const Popup = () => {
       <button
         onClick={() => {
           chrome.runtime.sendMessage({}, (res) => {
-            fetch("https://api.twitter.com/2/oauth2/token", {
-              method: "POST",
-              headers: {
-                Authorization: `Basic ZFU5TVFYTk9kMlpFWTJaeVkxZHZNR3BCTmxnNk1UcGphUToyRVNFR3h3T0dXQ09Vdjl4OHNMWTA4UnBTMVNzQUllNXloaEE3UTlsdU9INFlfQUw2Ug==`,
-                "Content-type": "application/x-www-form-urlencoded",
-              },
-              body: new URLSearchParams({
-                code: res!,
-                grant_type: "authorization_code",
-                client_id: API_KEY,
-                redirect_uri: REDIRECT_URL,
-                code_verifier: "abcd",
-              }),
-            })
-              .then((res) => {
-                console.log(res);
-                try {
-                } catch (e) {
-                  console.log(e);
+            /*axios
+              .post(
+                "https://api.twitter.com/2/oauth2/token",
+                new URLSearchParams({
+                  code: res!,
+                  grant_type: "authorization_code",
+                  client_id: API_KEY,
+                  redirect_uri: REDIRECT_URL,
+                  code_verifier: "abcd",
+                }),
+                {
+                  headers: {
+                    Authorization: `Basic ZFU5TVFYTk9kMlpFWTJaeVkxZHZNR3BCTmxnNk1UcGphUToyRVNFR3h3T0dXQ09Vdjl4OHNMWTA4UnBTMVNzQUllNXloaEE3UTlsdU9INFlfQUw2Ug==`,
+                    "Content-type": "application/x-www-form-urlencoded",
+                  },
+                  withCredentials: true,
                 }
-              })
-              .catch((e) => {
-                console.log(e);
-              });
+              )
+              .then((res) => console.log(res));*/
           });
         }}
       >
